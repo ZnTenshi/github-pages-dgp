cf8f6988f87eae954af1f595df2bc1d810c73c06
ZnTenshi <59067434+ZnTenshi@users.noreply.github.com>
2024-07-04 15:20:18 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-document.domain = "172.27.5.26";
+//document.domain = "172.27.5.26";
 alert("Alive!");
-console.log(window.parent.getAllAngularRootElements());
+//console.log(window.parent.getAllAngularRootElements());
+function displayMessage (evt) {
+	var message;
+	if (evt.origin !== "https://robertnyman.com") {
+	  message = "I got " + evt.data + " from " + evt.origin;
+	}
+	alert(message);
+}
+
+if (window.addEventListener) {
+	// For standards-compliant web browsers
+	window.addEventListener("message", displayMessage, false);
+}
+else {
+	window.attachEvent("onmessage", displayMessage);
+}
