5421888f842ccfc246fff73b331ab2883089bae6
ZnTenshi <59067434+ZnTenshi@users.noreply.github.com>
2024-07-04 18:51:18 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,44 @@
+function xhr (type, url, data, options) {
+  options = options || {};
+  var request = new XMLHttpRequest();
+  request.open(type, url, true);
+  if(type === "POST"){
+      request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+  }
+  request.onreadystatechange = function () {
+    if (this.readyState === 4) {
+      if (this.status >= 200 && this.status < 400) {
+        options.success && option.success(parse(this.responseText));
+      } else {
+        options.error && options.error(this.status);
+      }
+    }
+  };
+  request.send(data);
+}
+
+function parse(text){
+  try {
+     return JSON.parse(text);
+  } catch(e){
+     return text;
+  }
+}
+
 //document.domain = "172.27.5.26";
 //console.log(window.parent.getAllAngularRootElements());
 function displayMessage (evt) {
-	const target = document.getElementById("selected-id");
-	if (target) {
-		target.innerHTML = evt.data;	
+	const idNode = document.getElementById("selected-id");
+	if (idNode) {
+		idNode.innerHTML = evt.data;		
 	}
+	xhr("POST", 'http://172.27.5.26:81/app/oasimka/cards/api/FormInstance/5', [{id: 1, value: [evt.data]}], {success: (response) => {
+		const jsonNode = document.getElementById("selected-json");
+		if (jsonNode) {
+			jsonNode.innerHTML = JSON.stringify(response, null, 2);
+			
+		}		
+	}}	
 }
 
 if (window.addEventListener) {
